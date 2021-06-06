6ab980fbad2af5097980abcfed76d138914d4e3c
nvshah <nipunshah6776@gmail.com>
2021-06-07 00:33:59 +0530

    MongoDb Server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd31b0525da2cd636f798e5823c5bf106'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '629e87fe-789e-46cc-895f-758b78a6d23a'
+};
